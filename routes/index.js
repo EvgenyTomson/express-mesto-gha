3197674214728ffb5b6f48f0eff23d9fd5d72083
EvgenyTomson <tomilin.evgeny@gmail.com>
2023-04-29 11:22:42 +0300

    fix: добавить роут для 404

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,6 @@ const usersRouter = require('./users');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
+router.use('/*', (req, res) => res.status(404).send({ message: 'Запись не найдена!' }));
 
 module.exports = router;
