b8bd1ff61b10a500834c81e767640be81f70540f
EvgenyTomson <tomilin.evgeny@gmail.com>
2023-05-09 12:47:00 +0300

    refactor: перенести обработку всех запросов из app.js в корневой роутер

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors } = require('celebrate');
 
 const { PORT, DB_URI } = require('./config');
 
 const app = express();
 
-const { login, createUser } = require('./controllers/users');
-
-const authMiddleware = require('./middlewares/auth');
 const catchErrorsMiddleware = require('./middlewares/catchErrors');
-const { createUserJoi, loginJoi } = require('./middlewares/celebrate');
 
 app.use(express.json());
 
 mongoose.connect(DB_URI, {});
 
-app.post('/signin', loginJoi, login);
-app.post('/signup', createUserJoi, createUser);
-
-app.use(authMiddleware);
 app.use('/', require('./routes/index'));
-
-app.use(errors({ message: 'Ошибка валидации данных!' }));
 app.use(catchErrorsMiddleware);
 
 app.listen(PORT, () => {});
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 const NotFoundError = require('../errors/notFoundError');
+const { errors } = require('celebrate');
 
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
 
+const { login, createUser } = require('../controllers/users');
+const { createUserJoi, loginJoi } = require('../middlewares/celebrate');
+const authMiddleware = require('../middlewares/auth');
+
+router.post('/signin', loginJoi, login);
+router.post('/signup', createUserJoi, createUser);
+
+router.use(authMiddleware);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 router.use('/*', (req, res, next) => next(new NotFoundError('Запись не найдена.')));
+router.use(errors({ message: 'Ошибка валидации данных!' }));
 
 module.exports = router;
